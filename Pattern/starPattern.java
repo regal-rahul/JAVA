5bb0a69c6728ff27803ce773b3fb956701f6db4b
regal-rahul <regalrahul777@gmail.com>
2022-08-06 13:36:05 +0530

    basic star printing without loops

--- Pattern/starPattern.java
+++ Pattern/starPattern.java
@@ -1,5 +1,14 @@
-package Pattern;
-
 public class starPattern {
-
+    public static void main(String[] args) {
+        // output
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        // output
+        System.out.print("*\n");
+        System.out.print("**\n");
+        System.out.print("***\n");
+        System.out.print("****\n");
+    }
 }
