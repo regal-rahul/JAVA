1288e3b3aa0ade96c998089eab0cef37c15e7a4b
regal-rahul <regalrahul777@gmail.com>
2022-08-06 12:59:57 +0530

    diff types of printing

--- 01_Hello_World/HelloWorld.java
+++ 01_Hello_World/HelloWorld.java
@@ -1,5 +1,12 @@
-class HelloWorld {
-    public static void main(String args[]) {
+public class HelloWorld {
+    public static void main(String[] args) {
+        // output
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        // output to next line with using \n
+        System.out.print("Hello World\n");
+        System.out.print("Hello World\nfrom\nRahul\n");
+        // output without jumping to next line
+        System.out.print("Hello World");
     }
 }
