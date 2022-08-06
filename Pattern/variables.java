b5ee74b415384b96ca0c5ee5ce00def110844252
regal-rahul <regalrahul777@gmail.com>
2022-08-06 13:36:14 +0530

    variables

---
+++ Pattern/variables.java
@@ -0,0 +1,28 @@
+public class variables {
+    public static void main(String[] args) {
+        // variables
+        String name = "Tony Stark";
+        int age = 48;
+        double price = 25.9;
+
+        int a = 18;
+        int b = 9;
+
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        int div = a / b;
+
+        // TODO AxB/A-B
+        int algebra = (a * b) / (a - b);
+
+        // System.out.println(name);
+        // System.out.println(age);
+        // System.out.println(price);
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(algebra);
+    }
+}
