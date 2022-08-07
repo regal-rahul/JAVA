ce15ec954ea909c3c2e7f397c26172cc6a81191d
regal-rahul <regalrahul777@gmail.com>
2022-08-07 12:24:17 +0530

    half pyramid patter with numbers

--- patterns.java
+++ patterns.java
@@ -60,5 +60,15 @@ public class patterns {
         // System.out.println();
         // }
 
+        // TODO print number pattern in half pyramid structure
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 }
