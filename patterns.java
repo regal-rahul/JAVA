9688087fd637fecddb919bc265e3dfebc2919f2d
regal-rahul <regalrahul777@gmail.com>
2022-08-07 12:09:25 +0530

    half pyramind and inverted half pyramid

---
+++ patterns.java
@@ -0,0 +1,49 @@
+public class patterns {
+    public static void main(String[] args) {
+
+        // TODO print a rectangle made up of *
+        // for (int i = 1; i <= 4; i++) {
+        // for (int j = 1; j <= 5; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println("");
+        // }
+
+        // TODO Rectangular box with boundary only
+        // another pattern
+        // int n = 4;
+        // int m = 5;
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= m; j++) {
+        // if (i == 1 || j == 1 || i == n || j == m) {
+        // System.out.print("*");
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
+        // }
+
+        // TODO half pyramid
+        // int n = 4;
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // TODO inverted half pyramid
+
+        int n = 4;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
