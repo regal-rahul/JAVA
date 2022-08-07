d9f6803d529c99b308363d09234287c0b8bf45b5
regal-rahul <regalrahul777@gmail.com>
2022-08-07 10:24:48 +0530

    for and while loop

---
+++ loops.java
@@ -0,0 +1,39 @@
+public class loops {
+    public static void main(String[] args) {
+
+        // TODO FOR LOOP
+        // for (int i = 0; i < 10; i++) {
+        // System.out.println("Hello World");
+        // }
+
+        // TODO print numbers from 0 to 10
+
+        // Recommended solution
+        // for (int i = 0; i <= 10; i++) {
+        // System.out.println(i);
+        // }
+
+        // Another solution
+        // for (int i = 0; i < 11; i++) {
+        // System.out.println(i);
+        // }
+
+        // another solution
+        // for (int i = 0; i <= 10; i++)
+        // System.out.println(i);
+
+        // TODO WHILE LOOP
+
+        // int i = 0;
+        // while (i < 11) {
+        // System.out.println(i);
+        // i = i + 1;
+        // }
+
+        int i = 0;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
