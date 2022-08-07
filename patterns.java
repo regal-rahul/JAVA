e51a6e47149ad5c67f1ddedf35f6f3d28a194f24
regal-rahul <regalrahul777@gmail.com>
2022-08-07 12:32:03 +0530

    inverted number pyramid

--- patterns.java
+++ patterns.java
@@ -62,13 +62,45 @@ public class patterns {
 
         // TODO print number pattern in half pyramid structure
 
-        int n = 5;
+        // int n = 5;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // TODO inverted half pyramid with numbers
+        // 5 4 3 2 1
+        // 4 3 2 1
+        // 3 2 1
+        // 2 1
+        // 1
+
+        // int n = 5;
+
+        // for (int i = n; i >= 1; i--) {
+        // for (int j = i; j >= 1; j--) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
+
+        // TODO inverted half pyramid with numbers
+        // 1 2 3 4 5
+        // 1 2 3 4
+        // 1 2 3
+        // 1 2
+        // 1
+
+        // int n = 5;
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= n - i + 1; j++) {
+        // System.out.print(j + " ");
+        // }
+        // System.out.println();
+        // }
     }
 }
