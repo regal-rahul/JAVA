b63a26431cabdc673b04644410a3b9029cb4f003
regal-rahul <regalrahul777@gmail.com>
2022-08-07 12:35:40 +0530

    flyods traingle

--- patterns.java
+++ patterns.java
@@ -102,5 +102,17 @@ public class patterns {
         // }
         // System.out.println();
         // }
+
+        // TODO Floyd's Triangle
+        int n = 5;
+        int num = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
     }
 }
