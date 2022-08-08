0afd33dba6a57e115901e9a762792c5d106eed68
regal-rahul <regalrahul777@gmail.com>
2022-08-08 11:27:03 +0530

    diamond pattern

--- advancePattern.java
+++ advancePattern.java
@@ -74,20 +74,47 @@ public class advancePattern {
 
         // TODO palindrome pattern
 
-        int n = 5;
+        // int n = 5;
 
+        // for (int i = 1; i <= n; i++) {
+        // space
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // 1st half
+        // for (int j = i; j >= 1; j--) {
+        // System.out.print(j);
+        // }
+        // 2nd half
+        // for (int j = 2; j <= i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+
+        // TODO diamond pattern
+
+        int n = 4;
         for (int i = 1; i <= n; i++) {
-            // space
+            // spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            // 1st half
-            for (int j = i; j >= 1; j--) {
-                System.out.print(j);
+            // stars
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
             }
-            // 2nd half
-            for (int j = 2; j <= i; j++) {
-                System.out.print(j);
+            System.out.println();
+        }
+        // lower half
+        for (int i = n; i >= 1; i--) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
