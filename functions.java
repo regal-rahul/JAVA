74fa88fa8a8152a6d3f4fafbc866888ba0b125c2
regal-rahul <regalrahul777@gmail.com>
2022-08-08 12:15:00 +0530

    performing arithmatics with function call

--- functions.java
+++ functions.java
@@ -19,9 +19,25 @@ public class functions {
 
     // TODO write a function for sum of 2 numbers.
 
-    public static int calculateSum(int a, int b) {
-        int sum = a + b;
-        return sum;
+    // public static int calculateSum(int a, int b) {
+    // int sum = a + b;
+    // return sum;
+    // }
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+
+    // int sum = calculateSum(a, b);
+    // System.out.println("Sum of 2 numbers is: " + sum);
+    // }
+
+    // TODO make a function to multiply 2 numbers and return the product
+
+    public static int calculateMultiply(int a, int b) {
+        int multiply = a * b;
+        return multiply;
     }
 
     public static void main(String[] args) {
@@ -29,7 +45,8 @@ public class functions {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        int sum = calculateSum(a, b);
-        System.out.println("Sum of 2 numbers is: " + sum);
+        int multiply = calculateMultiply(a, b);
+
+        System.out.println("Product of the two number is: " + multiply);
     }
 }
