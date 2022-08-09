31d7cee55a58892f424a742a917d560a96d4f952
regal-rahul <regalrahul777@gmail.com>
2022-08-09 11:25:06 +0530

    1-5 solved

---
+++ firstProblem.java
@@ -0,0 +1,187 @@
+import java.util.*;
+
+public class firstProblem {
+    // 1
+    // Try to declare meaningful variables of each type. Eg - a variable named age
+    // should be a numeric type (int or float) not byte.
+
+    // public static void main(String[] args) {
+    // String name = "Rahul Kumar";
+    // int age = 23;
+    // float height = 5.67f;
+    // boolean bool = true;
+    // char grade = 'A';
+    // byte byteNum = 111;
+    // double dNum = 9.943534857345d;
+    // long lNum = 9128472387234273489L;
+    // short sNum = 29999;
+
+    // System.out.println(name + " is " + age + " yrs old and " + height + " feet
+    // height");
+    // System.out.println("Boolean: " + bool);
+    // System.out.println("grade: " + grade);
+    // System.out.println("byteNum: " + byteNum + ", dNum: " + dNum + ", lNum: " +
+    // lNum + ", sNum: " + sNum);
+    // }
+
+    // 2
+    // Make a program that takes the radius of a circle as input, calculates its
+    // radius and area and prints it as output to the user.
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // float rad = sc.nextFloat();
+
+    // float pi = 3.14f;
+
+    // float diameter = 2 * rad;
+    // float circumference = 2 * (pi * rad);
+    // float area = pi * (rad * rad);
+
+    // System.out.println("Diameter of the circle is: " + diameter);
+    // System.out.println("Circumference of the circle is: " + circumference);
+    // System.out.println("Area of the circle: " + area);
+    // }
+
+    // 3
+    // Make a program that prints the table of a number that is input by the user.
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+    // int num = sc.nextInt();
+
+    // System.out.println(num * 1);
+    // System.out.println(num * 2);
+    // System.out.println(num * 3);
+    // System.out.println(num * 4);
+    // System.out.println(num * 5);
+    // System.out.println(num * 6);
+    // System.out.println(num * 7);
+    // System.out.println(num * 8);
+    // System.out.println(num * 9);
+    // System.out.println(num * 10);
+    // }
+
+    // 4
+    // Make a Calculator. Take 2 numbers (a & b) from the user and an operation as
+    // follows :
+    // 1 : + (Addition) a + b
+    // 2 : - (Subtraction) a - b
+    // 3 : * (Multiplication) a * b
+    // 4 : / (Division) a / b
+    // 5 : % (Modulo or remainder) a % b
+    // Calculate the result according to the operation given and display it to the
+    // user.
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+
+    // System.out.print("Enter first number: ");
+    // int a = sc.nextInt();
+    // System.out.print("Enter second number: ");
+    // int b = sc.nextInt();
+
+    // System.out.println();
+
+    // System.out.println(
+    // "List of operation you can perform:\n1: Add\n2: Subtract\n3: Multiply\n4:
+    // Division\n5: Modulo or Remainder");
+    // System.out.print("Choose a number to perform operation: ");
+    // int operation = sc.nextInt();
+
+    // System.out.println();
+    // switch (operation) {
+    // case 1:
+    // int sum = a + b;
+    // System.out.println("Sum: " + sum);
+    // break;
+    // case 2:
+    // int sub = a - b;
+    // System.out.println("Diffrence: " + sub);
+    // break;
+    // case 3:
+    // int mul = a * b;
+    // System.out.println("Product: " + mul);
+    // break;
+    // case 4:
+    // int div = a / b;
+    // System.out.println("Quotient: " + div);
+    // break;
+    // case 5:
+    // int mod = a % b;
+    // System.out.println("Remainder: " + mod);
+    // break;
+
+    // default:
+    // System.out.println("Invalid Input!! Run the program again.");
+    // }
+    // }
+
+    // 5
+    // Ask the user to enter the number of the month & print the name of the month.
+    // For eg - For ‘1’ print ‘January’, ‘2’ print ‘February’ & so on.
+
+    // public static void main(String[] args) {
+    // Scanner sc = new Scanner(System.in);
+
+    // System.out.print("Enter the month number to find out the name: ");
+    // int month = sc.nextInt();
+    // System.out.println();
+
+    // switch (month) {
+    // case 1:
+    // System.out.println(month + ": January");
+    // break;
+    // case 2:
+    // System.out.println(month + ": February");
+    // break;
+    // case 3:
+    // System.out.println(month + ": March");
+    // break;
+    // case 4:
+    // System.out.println(month + ": April");
+    // break;
+    // case 5:
+    // System.out.println(month + ": May");
+    // break;
+    // case 6:
+    // System.out.println(month + ": June");
+    // break;
+    // case 7:
+    // System.out.println(month + ": July");
+    // break;
+    // case 8:
+    // System.out.println(month + ": August");
+    // break;
+    // case 9:
+    // System.out.println(month + ": September");
+    // break;
+    // case 10:
+    // System.out.println(month + ": October");
+    // break;
+    // case 11:
+    // System.out.println(month + ": November");
+    // break;
+    // case 12:
+    // System.out.println(month + ": December");
+    // break;
+
+    // default:
+    // System.out.println("Invalid Input!! Restart the program again.");
+    // break;
+    // }
+    // }
+
+    // 6
+    // Print all even numbers till n.
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    // 7
+    // 8
+    // 9
+    // 10
+}
