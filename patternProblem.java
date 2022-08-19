44f744cd47c90d78914ea827add266ec61a4a8c3
regal-rahul <regalrahul777@gmail.com>
2022-08-19 13:04:39 +0530

    inverted same number pattern

--- patternProblem.java
+++ patternProblem.java
@@ -155,12 +155,25 @@ public class patternProblem {
 
     // 7
     // Print half number pyramid with increasing number in every line.
+    // public static void main(String[] args) {
+    // int n = 5;
+
+    // for (int i = 1; i <= n; i++) {
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print(j);
+    // }
+    // System.out.println();
+    // }
+    // }
+
+    // 8
+    // Print inverted half pyramid with same number in a single row.
     public static void main(String[] args) {
         int n = 5;
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+            for (int j = n; j >= i; j--) {
+                System.out.print(i);
             }
             System.out.println();
         }
