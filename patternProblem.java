b2dc403b35c134758c778c303f1f6702e1a0ccef
regal-rahul <regalrahul777@gmail.com>
2022-08-19 12:42:39 +0530

    hollow rhombus

--- patternProblem.java
+++ patternProblem.java
@@ -2,7 +2,7 @@ public class patternProblem {
     // 1
     // Print a solid rhombus.
     // public static void main(String[] args) {
-    // int n = n;
+    // int n = 5;
 
     // for (int i = 1; i <= n; i++) {
     // // spaces
@@ -21,7 +21,7 @@ public class patternProblem {
     // 2
     // Print a number pyramid.
     // public static void main(String[] args) {
-    // int n = n;
+    // int n = 5;
 
     // for (int i = 1; i <= n; i++) {
     // // spaces
@@ -39,7 +39,7 @@ public class patternProblem {
     // 3
     // Print a palindromic number pyramid.
     // public static void main(String[] args) {
-    // int n = n;
+    // int n = 5;
 
     // for (int i = 1; i <= n; i++) {
     // // spaces
@@ -60,55 +60,75 @@ public class patternProblem {
     // }
     // }
 
-    // m
+    // 4
     // Print a hollow butterfly pattern
+    // public static void main(String[] args) {
+    // int n = 5;
+    // int m = 4;
+    // for (int j = 1; j <= n; j++) { // 1st outer loop
+
+    // for (int k = 1; k <= j; k++) { // Star Printing - 2nd inner loop
+    // if (k == 1 || k == j)
+    // System.out.print("*");
+    // else
+    // System.out.print(" ");
+    // }
+
+    // for (int k = n; k >= j; k--) { // Spaces
+    // System.out.print(" ");
+    // }
+
+    // for (int k = 1; k <= j; k++) { // Star Printing - 2nd inner loop
+    // if (k == 1 || k == j)
+    // System.out.print("*");
+    // else
+    // System.out.print(" ");
+    // }
+    // System.out.println();
+    // }
+
+    // for (int j = 1; j <= m; j++) { // 2nd outer for loop
+
+    // for (int k = m; k >= j; k--) { // Stars printing - 2nd inner for
+    // // loop
+    // if (k == m || k == j)
+    // System.out.print("*");
+    // else
+    // System.out.print(" ");
+
+    // }
+
+    // for (int k = 0; k <= j; k++) { // Spaces
+    // System.out.print(" ");
+    // }
+
+    // for (int k = m; k >= j; k--) { // Star Printing - 2nd inner loop
+    // if (k == m || k == j)
+    // System.out.print("*");
+    // else
+    // System.out.print(" ");
+    // }
+
+    // System.out.println();
+    // }
+    // }
+
+    // 5
+    // Print a hollow Rhombus.
     public static void main(String[] args) {
         int n = 5;
-        int m = 4;
-        for (int j = 1; j <= n; j++) { // 1st outer loop
 
-            for (int k = 1; k <= j; k++) { // Star Printing - 2nd inner loop
-                if (k == 1 || k == j)
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (j == 1 || i == 1 || i == n || j == n) {
                     System.out.print("*");
-                else
+                } else {
                     System.out.print(" ");
+                }
             }
-
-            for (int k = n; k >= j; k--) { // Spaces
-                System.out.print("  ");
-            }
-
-            for (int k = 1; k <= j; k++) { // Star Printing - 2nd inner loop
-                if (k == 1 || k == j)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
-            }
-            System.out.println();
-        }
-
-        for (int j = 1; j <= m; j++) { // 2nd outer for loop
-
-            for (int k = m; k >= j; k--) { // Stars printing - 2nd inner for
-                                           // loop
-                if (k == m || k == j)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
-
-            }
-
-            for (int k = 0; k <= j; k++) { // Spaces
-                System.out.print("  ");
-            }
-
-            for (int k = m; k >= j; k--) { // Star Printing - 2nd inner loop
-                if (k == m || k == j)
-                    System.out.print("*");
-                else
-                    System.out.print(" ");
-            }
-
             System.out.println();
         }
     }
