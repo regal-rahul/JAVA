f3a483222dabecceebaa882e1c6aa6995d77e35e
regal-rahul <regalrahul777@gmail.com>
2022-08-19 13:01:36 +0530

    half no. pyramid in increasing order

--- patternProblem.java
+++ patternProblem.java
@@ -135,19 +135,32 @@ public class patternProblem {
 
     // 6
     // Print pascal triangle
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
 
+    // int n = 5;
+
+    // for (int i = 1; i <= n; i++) {
+    // // spacing
+    // for (int j = n; j >= i; j--) {
+    // System.out.print(" ");
+    // }
+    // int c = 1;
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print(c + " ");
+    // c = c * (i - j) / j;
+    // }
+    // System.out.println();
+    // }
+    // }
+
+    // 7
+    // Print half number pyramid with increasing number in every line.
+    public static void main(String[] args) {
         int n = 5;
 
         for (int i = 1; i <= n; i++) {
-            // spacing
-            for (int j = n; j >= i; j--) {
-                System.out.print(" ");
-            }
-            int c = 1;
             for (int j = 1; j <= i; j++) {
-                System.out.print(c + " ");
-                c = c * (i - j) / j;
+                System.out.print(j);
             }
             System.out.println();
         }
