d8d9931433cea0cf1b9b8518f4815eba535798af
regal-rahul <regalrahul777@gmail.com>
2022-08-24 16:49:35 +0530

    string substring

---
+++ string.java
@@ -0,0 +1,43 @@
+// import java.util.*;
+
+public class string {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+
+        // String name = sc.nextLine();
+        // sc.close();
+        // System.out.println("Your name is: " + name);
+
+        // concatenation
+        // String fName = "Captain";
+        // String lName = "America";
+
+        // String fullName = fName + " " + lName;
+
+        // // System.out.println(fullName);
+        // System.out.println(fullName.length());
+
+        // // charAt
+        // for (int i = 0; i < fullName.length(); i++) {
+        // System.out.println(fullName.charAt(i));
+        // }
+
+        // string compare
+        // String name1 = "Hulk";
+        // String name2 = "Hulk";
+
+        // if (name1.compareTo(name2) == 0) {
+        // System.out.println("Both are same.");
+        // } else {
+        // System.out.println("Both are not same.");
+        // }
+
+        // substring
+
+        String sentence = "My name is Rahul";
+        // String name = sentence.substring(11, sentence.length());
+        String name = sentence.substring(11);
+
+        System.out.println(name);
+    }
+}
